e7be5bd9c526d8d8dd1f70d7391f1772e64703af
César Lawall <clawall@gmail.com>
2019-11-24 16:58:17 -0300

    Exercício 7.7

--- app/src/main/java/br/com/caelum/twittelumappweb/activity/MainActivity.kt
+++ app/src/main/java/br/com/caelum/twittelumappweb/activity/MainActivity.kt
@@ -2,12 +2,42 @@ package br.com.caelum.twittelumappweb.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import br.com.caelum.twittelumappweb.R
+import br.com.caelum.twittelumappweb.fragment.ListaTweetsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_navigation.selectedItemId = R.id.menu_tweets
+
+        listenerBottomNavigation()
+    }
+
+    private fun listenerBottomNavigation() {
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_tweets -> {
+                    exibe(ListaTweetsFragment())
+
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    private fun exibe(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+
+        transaction.replace(R.id.frame_principal, fragment)
+
+        transaction.commit()
     }
 }
